e57800976e6cfc5afea1f259c10d6c46e6b2d64f
HidekiHiguchi <hideki-higuchi@outlook.com>
2015-10-13 01:41:47 -0300

    Dudu meu espartilho

--- index.php
+++ index.php
@@ -1,63 +1,6 @@
-<!DOCTYPE HTML>
-<html>
 
-<head>
-  <title>Simple Auction House</title>
-  <meta name="description" content="Simple LightWeight Auction Site" />
-  <meta name="Leilão" content="Auction Site, Auction House" />
-  <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
-  <link rel="stylesheet" type="text/css" href="style/style.css" title="style" />
-</head>
-
-<body>
-  <div id="main">
-    <div id="header">
-      <div id="logo">
-        <div id="logo_text">
-          <!-- class="logo_colour", allows you to change the colour of the text -->
-          <h1><a href="index.html">textured<span class="logo_colour">blue</span></a></h1>
-          <h2>Auction House.</h2>
-        </div>
-      </div>
-      <div id="menubar">
-        <ul id="menu">
-          <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
-          <li class="selected"><a href="index.html">Home</a></li>
-          <li><a href="examples.html">Examples</a></li>
-          <li><a href="page.html">A Page</a></li>
-          <li><a href="another_page.html">Another Page</a></li>
-          <li><a href="contact.html">Contact Us</a></li>
-        </ul>
-      </div>
-    </div>
-    <div id="site_content">
-      <div class="sidebar">
-        <!-- insert your sidebar items here -->
-        <h3>Latest News</h3>
-        <h4>New Website Launched</h4>
-        <h5>August 1st, 2013</h5>
-        <p>2013 sees the redesign of our website. Take a look around and let us know what you think.<br /><a href="#">Read more</a></p>
-        <p></p>
-        <h4>New Website Launched</h4>
-        <h5>August 1st, 2013</h5>
-        <p>2013 sees the redesign of our website. Take a look around and let us know what you think.<br /><a href="#">Read more</a></p>
-        <h3>Useful Links</h3>
-        <ul>
-          <li><a href="#">link 1</a></li>
-          <li><a href="#">link 2</a></li>
-          <li><a href="#">link 3</a></li>
-          <li><a href="#">link 4</a></li>
-        </ul>
-        <h3>Search</h3>
-        <form method="post" action="#" id="search_form">
-          <p>
-            <input class="search" type="text" name="search_field" value="Enter keywords....." />
-            <input name="search" type="image" style="border: 0; margin: 0 0 -9px 5px;" src="style/search.png" alt="Search" title="Search" />
-          </p>
-        </form>
-      </div>
-      <div id="content">
-        <!-- insert the page content here -->
+<?php include 'includes/siteUp.php';?>
+  <!-- all content bellow-->
         <h1>Welcome to the textured_blue template</h1>
         <img src="style/ie.png">
         <p>This standards compliant, simple, fixed width website template is released as an 'open source' design (under a <a href="http://creativecommons.org/licenses/by/3.0">Creative Commons Attribution 3.0 Licence</a>), which means that you are free to download and use it for anything you want (including modifying and amending it). All I ask is that you leave the 'design from HTML5webtemplates.co.uk' link in the footer of the template, but other than that...</p>
@@ -72,11 +15,5 @@
           <li>FireFox 3.5</li>
           <li>Google Chrome 6</li>
         </ul>
-      </div>
-    </div>
-    <div id="footer">
-        <img src="style/created-with-vim.gif">
-    </div>
-  </div>
-</body>
-</html>
+  <!-- content end here -->
+<?php include 'includes/siteDown.php';?>
