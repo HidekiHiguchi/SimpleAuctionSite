99fc46f3a510c78d1e25573a94284387a6f54769
HidekiHiguchi <hideki-higuchi@outlook.com>
2015-10-13 16:28:55 -0300

    a

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php include 'includes/siteUp.php';?>
+<!-- all content bellow-->
+<img src="style/ie.png">
+<h2>Browser Compatibility</h2>
+<ul>
+  <li>Internet Explorer 8</li>
+  <li>Internet Explorer 7</li>
+  <li>FireFox 3.5</li>
+  <li>Google Chrome 6</li>
+</ul>
+<!-- content end here -->
+<?php include 'includes/siteDown.php';?>
---
+++ category.php
@@ -0,0 +1,5 @@
+<?php include 'includes/siteUp.php';?>
+<!-- all content bellow-->
+
+<!-- content end here -->
+<?php include 'includes/siteDown.php';?>
